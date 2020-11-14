9cd0c8ead49a675e1313feeb7cc4b1b96fd13d26
Tom Haigh <tmhaigh@gmail.com>
2020-11-14 14:39:00 +1100

    added html route for create recipe

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -26,6 +26,10 @@ module.exports = function(app) {
         res.render("index");
     })
 
+    app.get("/create-recipe", function(req, res) {
+        res.render("create-recipe");
+    })
+
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be redirected to the signup page
     app.get("/members", isAuthenticated, function(req, res) {
