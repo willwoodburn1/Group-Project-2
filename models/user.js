c036b543a45145843633236093ad83adde736a72
Tom Haigh <tmhaigh@gmail.com>
2020-11-17 10:47:29 +1100

    removed email from user table

--- models/user.js
+++ models/user.js
@@ -2,18 +2,10 @@ var bcrypt = require("bcryptjs");
 
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
-        name: {
+        username: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true
-            }
         },
         password: {
             type: DataTypes.STRING,
