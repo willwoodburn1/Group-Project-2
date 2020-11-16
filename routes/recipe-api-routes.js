b17d09ac0725f61bcfcc56977841a4cce9a0253f
Tom Haigh <tmhaigh@gmail.com>
2020-11-16 13:16:15 +1100

    added basic get api's for the different databases

---
+++ routes/recipe-api-routes.js
@@ -0,0 +1,49 @@
+// Requiring our models and passport as we've configured it
+var db = require("../models");
+
+module.exports = function(app) {
+
+    // get recipe and ingredients list with the recipe_id
+    app.get("api/recipesIngredients/:id", function(req, res) {
+        db.recipesIngredients.findAll({
+            where: {
+                recipes_id: req.params.id
+            }
+        }).then(function(data) {
+            res.json(data);
+        })
+    })
+
+    // get chef
+    app.get("api/chef/:id", function(req, res) {
+        db.chef.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(data) {
+            res.json(data);
+        })
+    })
+
+    // get recipe
+    app.get("api/recipes/:id", function(req, res) {
+        db.recipes.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(data) {
+            res.json(data);
+        })
+    })
+
+    // get measures
+    app.get("api/measure/:id", function(req, res) {
+        db.measure.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(data) {
+            res.json(data);
+        })
+    })
+}
