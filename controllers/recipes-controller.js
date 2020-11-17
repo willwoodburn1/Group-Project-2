f871551d161b08ba1758e3328b2504bb8fb8c6a0
Tom Haigh <tmhaigh@gmail.com>
2020-11-18 10:21:08 +1100

    replaced db.Recipe with Recipe

--- controllers/recipes-controller.js
+++ controllers/recipes-controller.js
@@ -1,21 +1,24 @@
 const sequelize = require("sequelize");
-const { Op } = require('sequelize')
+const { Op } = require("sequelize");
 
 // Requiring our models
 var db = require("../models");
+
+// Tip for you Tom
+const { Recipe } = require("../models");
 
 module.exports = function(app) {
 
     // get all the recipes
     app.get("/api/recipes", function(req, res) {
-        db.Recipe.findAll().then(function(data) {
+        Recipe.findAll().then(function(data) {
             res.json(data);
         })
     })
 
     // get a recipe using the recipe id
     app.get("api/recipes/:id", function(req, res) {
-        db.Recipe.findOne({
+        Recipe.findOne({
             where: {
                 id: req.params.id
             }
@@ -25,37 +28,34 @@ module.exports = function(app) {
     })
 
     app.get("/api/recipes/less-than/:price", function(req, res) {
-        console.log(db.Ingredients);
-        db.Recipe.findAll({
+        let price = req.params.price;
+        Recipe.findAll({
             include: [{
                 model: db.Ingredients,
                 through: "recipe_ingredients",
                 as: "ingredients",
                 required: true
             }],
-            attributes: ['recipe.title', [sequelize.fn('sum', sequelize.col('ingredients.price')), 'test'], ],
+            attributes: ["recipe.title", [sequelize.fn("sum", sequelize.col("ingredients.price")), "test"], ],
             group: ["recipe.title"],
-            // where: {
-            //     sequelize.where(sequelize.fn('sum', sequelize.col('ingredients.price')), 2)
-            // },
             having: {
-                'ingredients.price': sequelize.where(sequelize.fn('sum', sequelize.col('ingredients.price')), {
-                    [Op.lt]: req.params.price
+                "ingredients.price": sequelize.where(sequelize.fn("sum", sequelize.col("ingredients.price")), {
+                    [Op.lt]: price
                 })
             }
-        }).then(function(recipes) {
-            console.log(recipes);
+        }).then(function(data) {
+            res.json(data);
         })
     })
 
     app.post("api/recipes", function(req, res) {
-        db.Recipe.create(req.body).then(function(recipe) {
+        Recipe.create(req.body).then(function(recipe) {
             res.json(recipe);
         })
     });
 
     app.delete("/api/recipes/:id", function(req, res) {
-        db.Recipe.destroy({
+        Recipe.destroy({
             where: {
                 id: req.params.id
             }
