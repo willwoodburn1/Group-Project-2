f5f37feb3175e1f75d922c9ba08281990190691e
Tom Haigh <tmhaigh@gmail.com>
2020-11-17 13:53:12 +1100

    moved code to appropriate controller files

--- routes/recipe-api-routes.js
+++ routes/recipe-api-routes.js
@@ -1,48 +1,17 @@
-// Requiring our models and passport as we've configured it
+// Requiring our models
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
 
-    // get recipe and ingredients list with the recipe_id
-    app.get("api/recipesIngredients/:id", function (req, res) {
-        db.recipes_ingredients.findAll({
-            where: {
-                recipes_id: req.params.id
-            }
-        }).then(function (data) {
-            res.json(data);
-        })
-    })
 
-    // get chef
-    app.get("api/chef/:id", function (req, res) {
-        db.chef.findOne({
-            where: {
-                id: req.params.id
-            }
-        }).then(function (data) {
-            res.json(data);
-        })
-    })
-
-    // get recipe
-    app.get("api/recipes/:id", function (req, res) {
-        db.recipes.findOne({
-            where: {
-                id: req.params.id
-            }
-        }).then(function (data) {
-            res.json(data);
-        })
-    })
 
     // get measures
-    app.get("api/measure/:id", function (req, res) {
-        db.measure.findOne({
+    app.get("api/measure/:id", function(req, res) {
+        db.Measure.findOne({
             where: {
                 id: req.params.id
             }
-        }).then(function (data) {
+        }).then(function(data) {
             res.json(data);
         })
     })
