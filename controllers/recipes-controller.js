e56a91749a85ab409ed8b8564a334455285da3b9
Tom Haigh <tmhaigh@gmail.com>
2020-11-17 18:45:54 +1100

    added api/recipes route here

--- controllers/recipes-controller.js
+++ controllers/recipes-controller.js
@@ -3,7 +3,14 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-    // get recipe
+    // get all the recipes
+    app.get("api/recipes", function(req, res) {
+        db.Recipe.findAll().then(function(data) {
+            res.json(data);
+        })
+    })
+
+    // get a recipe using the recipe id
     app.get("api/recipes/:id", function(req, res) {
         db.Recipe.findOne({
             where: {
