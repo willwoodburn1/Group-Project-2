c5e21596925807607e7cd794f59ee1d08816fb02
User <user@Users-MacBook-Pro.local>
2020-11-17 18:32:35 +1000

    added delete functions to routes

--- controllers/recipes-controller.js
+++ controllers/recipes-controller.js
@@ -25,6 +25,16 @@ module.exports = function (app) {
         db.Recipe.create(req.body).then(function (recipe) {
             res.json(recipe);
         })
+    });
+
+    app.delete("/api/recipes/:id", function (req, res) {
+        db.Recipe.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (data) {
+            res.json(data)
+        })
     })
 
 }
