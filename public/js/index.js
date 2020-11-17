74a587d7dcd7c750dc347b393e01a5da8a024031
Tom Haigh <tmhaigh@gmail.com>
2020-11-18 10:52:27 +1100

    removed : from api/recipes get call

--- public/js/index.js
+++ public/js/index.js
@@ -18,9 +18,8 @@ $(document).ready(function() {
     })
 
     function findRecipes(budget) {
-        $.get(`/api/recipes/less-than/:${budget}`, function(data) {
-            console.log(budget);
-            console.log(data);
+        $.get(`/api/recipes/less-than/${budget}`, function(data) {
+
         })
     }
 
