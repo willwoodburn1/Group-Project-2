2b75d8a446f2274fc0d27a1b3c6cd2d43afc3a3c
Tom Haigh <tmhaigh@gmail.com>
2020-11-17 20:30:26 +1100

    added initial code for index.handlebars

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
 
+    let budgetForm = $("#budget-form");
+    let budgetInput = $("#budget-input");
+
+    budgetForm.on("submit", function(event) {
+        event.preventDefault();
+        let inputData = {
+            budget: budgetInput.val().trim()
+        }
+
+        if (!inputData.budget) {
+            return;
+        }
+
+        findRecipes(inputData.budget);
+        budgetInput.val("");
+    })
+
+    function findRecipes(budget) {
+        $.get("/api/recipes", function(data) {
+            console.log(data);
+        })
+    }
+
 })
+
+// How much would you like to spend?
+// Select or enter amount in dollars
+// Bring up recipes that cost that much to make or less
