133df2e91eaba29ef3773e52da2f75b5680464ec
Simone Cheah <simone_cheah@hotmail.com>
2020-11-19 09:03:58 +1100

    update find queries

--- controllers/recipe-ingredients-controller.js
+++ controllers/recipe-ingredients-controller.js
@@ -1,33 +1,51 @@
 // Requiring our models
 var db = require("../models");
+const sequelize = require("sequelize");
 
 module.exports = function (app) {
     // get recipe and ingredients list with the recipe_id
-    app.get("api/recipesIngredients/:id", function (req, res) {
-        db.recipes_ingredients.findAll({
-            where: {
-                recipes_id: req.params.id
-            }
-        }).then(function (data) {
+    app.get("/api/recipesIngredients/:recipe_id", function (req, res) {
+        db.sequelize.query(`
+        SELECT recipe_ingredients.ingredient_id, recipe_ingredients.recipe_id, recipe_ingredients.createdAt, recipe_ingredients.updatedAt
+            FROM recipe_ingredients
+        WHERE recipe_id = ${req.params.recipe_id};`, 
+        { 
+            type: sequelize.QueryTypes.SELECT 
+        }).then(function(data) {
+            res.json(data);
+        })
+    })
+
+    // get recipe id and ingredient id
+    app.get("/api/recipesIngredients", function(req, res) {
+        db.sequelize.query(`
+        SELECT recipe_ingredients.ingredient_id, recipe_ingredients.recipe_id, recipe_ingredients.createdAt, recipe_ingredients.updatedAt
+            FROM recipe_ingredients;`, 
+        { 
+            type: sequelize.QueryTypes.SELECT 
+        }).then(function(data) {
             res.json(data);
         })
     })
 
     // get all the recipes and ingredients
-    app.get("/api/recipesIngredients", function (req, res) {
-        db.recipes_ingredients.findAll().then(function (data) {
-            res.json(data);
-        })
-    });
+    // app.get("/api/recipesIngredients", function (req, res) {
+    //     db.recipe_ingredients.findAll().then(function (data) {
+    //         res.json(data);
+    //     })
+    // });
 
     app.post("/api/recipesIngredients", function (req, res) {
-        db.recipes_ingredients.create(req.body).then(function (data) {
+        db.recipe_ingredients.create({
+            ingredient_id: req.body.ingredient_id,
+            recipe_id: req.body.recipe_id,
+        }).then(function (data) {
             res.json(data);
         })
     });
 
-    app.delete("/api/ingredients/:id", function (req, res) {
-        db.recipes_ingredients.destroy({
+    app.delete("/api/recipesIngredients/:id", function (req, res) {
+        db.recipe_ingredients.destroy({
             where: {
                 recipes_id: req.params.id
             }
