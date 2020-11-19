cf11bcc9901eb3eead0719cff364e690f04bde68
Tom Haigh <tmhaigh@gmail.com>
2020-11-19 20:08:02 +1100

    added view recipes by user to members route

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -61,9 +61,14 @@ module.exports = function(app) {
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be redirected to the signup page
     app.get("/members", isAuthenticated, function(req, res) {
+        let userId = req.user.id;
         db.sequelize.query(`
-		
+		SELECT recipes.title
+		FROM recipes
+		JOIN users ON recipes.UserId=users.id
+		WHERE users.id=${userId};
 		`, { type: sequelize.QueryTypes.SELECT }).then(function(data) {
+            console.log(data);
             res.render("members", { recipes: data });
         })
     });
