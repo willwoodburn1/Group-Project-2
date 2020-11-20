0e07e467dbcf85d72c39c22642e428d0a11e2def
Simone Cheah <simone_cheah@hotmail.com>
2020-11-21 08:30:46 +1100

    validate duplicate items

--- public/js/create-recipe.js
+++ public/js/create-recipe.js
@@ -13,9 +13,9 @@ $(document).ready(function () {
 	var API_KEY = "";
 
 	// get api key
-	$.get("/api/spoonacular/key").then(function(KEY) {
+	$.get("/api/spoonacular/key").then(function (KEY) {
 		API_KEY = KEY;
-	})
+	});
 
 	// add recipe name
 	$("#add-recipe-name").on("click", function (event) {
@@ -57,83 +57,102 @@ $(document).ready(function () {
 
 		// search for ingredients in db
 		$.get(`/api/ingredients/search/${search}`).then(function (data) {
-			
 			for (var item of data) {
 				let ingredient = {
 					id: item.id,
 					name: item.item,
 					price: item.price,
-				}
+				};
 
 				$(".result-items").append(`
 				<button type="button" class="search-item">
 					<div class="results-id" id="${ingredient.id}"></div>
 					<div class="results-name" id="${ingredient.name}">${ingredient.name}</div>
 					<div id="${ingredient.price}">$<span class="results-price">${ingredient.price}</span></div>
-				</button>`)
+				</button>`);
 			}
-		})
+		});
 
 		// search for products in api
 		// api call to spoonacular - gets product name and price
-		// $.get(`https://api.spoonacular.com/food/products/suggest?query=${search}&number=10&apiKey=${API_KEY}`)
-		// .then(function (response) {
-		// 	for (var data of response.results) {
-
-		// 		// product details - gets price
-		// 		$.get(`https://api.spoonacular.com/food/products/${data.id}?apiKey=${API_KEY}`)
-		// 		.then(function(response) {
-					
-		// 			if (response.price) {
-		// 				let ingredient = {
-		// 					name: response.title,
-		// 					price: ((response.price / 100) * 1.37).toFixed(2) // USD * 1.37 = AUD
-		// 				}
-	
-		// 				$(".result-items").prepend(`
-		// 				<button type="button" class="search-item api">
-		// 					<div class="results-id"></div>
-		// 					<div class="results-name" id="${ingredient.name}">${ingredient.name}</div>
-		// 					<div id="${ingredient.price}">$<span class="results-price">${ingredient.price}</span></div>
-		// 				</button>`)
-		// 			}
-		// 		})
-		// 	}
-		// })
-
-		// search for ingredients in api - gets ingredient name and price
-		$.get(`https://api.spoonacular.com/food/ingredients/autocomplete?query=${search}&number=3&metaInformation=true&apiKey=${API_KEY}`)
+		$.get(`https://api.spoonacular.com/food/products/suggest?query=${search}&number=10&apiKey=${API_KEY}`)
 		.then(function (response) {
-			for (var data of response) {
+			for (let data of response.results) {
 
-				// ingredient details - gets price
-				$.get(`https://api.spoonacular.com/food/ingredients/${data.id}/information?amount=1&apiKey=${API_KEY}`)
+				// product details - gets price
+				$.get(`https://api.spoonacular.com/food/products/${data.id}?apiKey=${API_KEY}`)
 				.then(function(response) {
-					// USD * 1.37 = AUD
-					let ingredient = {
-						name: response.name,
-						price: ((response.estimatedCost.value / 100) * 1.37).toFixed(3)
-					}
 
-					$(".result-items").prepend(`
-					<button type="button" class="search-item api">
-						<div class="results-id"></div>
-						<div class="results-name" id="${ingredient.name}">${ingredient.name}</div>
-						<div id="${ingredient.price}">$<span class="results-price">${ingredient.price}</span></div>
-					</button>`)
+					if (response.price) {
+						let ingredient = {
+							name: response.title,
+							price: ((response.price / 100) * 1.37).toFixed(2) // USD * 1.37 = AUD
+						}
+						
+						$.get(`/api/ingredients/item/${ingredient.name}`).then(
+							function (data) {
+								if (data !== null) {
+									return;
+								} else {
+									$(".result-items").prepend(`
+								<button type="button" class="search-item api">
+									<div class="results-id"></div>
+									<div class="results-name" id="${ingredient.name}">${ingredient.name}</div>
+									<div id="${ingredient.price}">$<span class="results-price">${ingredient.price}</span></div>
+								</button>`);
+								}
+							}
+						);
+					}
 				})
 			}
 		})
+
+		// search for ingredients in api - gets ingredient name and price
+		$.get(
+			`https://api.spoonacular.com/food/ingredients/autocomplete?query=${search}&number=3&metaInformation=true&apiKey=${API_KEY}`
+		).then(function (response) {
+			for (let data of response) {
+				// ingredient details - gets price
+				$.get(
+					`https://api.spoonacular.com/food/ingredients/${data.id}/information?amount=1&apiKey=${API_KEY}`
+				).then(function (response) {
+					// USD * 1.37 = AUD
+					let ingredient = {
+						name: response.name,
+						price: (
+							(response.estimatedCost.value / 100) *
+							1.37
+						).toFixed(3),
+					};
+
+					$.get(`/api/ingredients/item/${ingredient.name}`).then(
+						function (data) {
+							if (data !== null) {
+								return;
+							} else {
+								$(".result-items").prepend(`
+							<button type="button" class="search-item api">
+								<div class="results-id"></div>
+								<div class="results-name" id="${ingredient.name}">${ingredient.name}</div>
+								<div id="${ingredient.price}">$<span class="results-price">${ingredient.price}</span></div>
+							</button>`);
+							}
+						}
+					);
+				});
+			}
+		});
 	});
 
-    // display in table - single ingredient id, name, price
-    // todo: display api results
+	// display in table - single ingredient id, name, price
+	// todo: display api results
 	$(".result-items").on("click", ".search-item", function (event) {
 		event.preventDefault();
 		$("#ingredient-id").text("");
 		$("#ingredient-name").text("");
 		$("#ingredient-price").text("");
-		
+
 		let name = event.currentTarget.children[1].id;
 		let price = event.currentTarget.children[2].id;
 		$("#ingredient-name").text(name);
@@ -143,10 +162,20 @@ $(document).ready(function () {
 			let id = event.currentTarget.children[0].id;
 			$("#ingredient-id").text(id);
 		} else {
-			// post ingredient to db
-			console.log("api class found")
+			// post new ingredient to db
+			console.log("api class found");
 
-			
+			$.get(`/api/ingredients/item/${name}`).then(function (data) {
+				console.log(data);
+				if (data !== null) {
+					return;
+				} else {
+					$.post("/api/ingredients", {
+						item: name,
+						price: price,
+					});
+				}
+			});
 		}
 		// todo: hide display table and add button until clicked on search item
 	});
@@ -154,34 +183,40 @@ $(document).ready(function () {
 	// add single ingredient to list
 	$("#add-ingredient").on("click", function (event) {
 		event.preventDefault();
-		let quantity = $("#ingredient-quantity").val().trim();
-		let measure = $("#ingredient-measures").val();
 
-		$.get(`/api/measures/${measure}`).then(function (data) {
-			let measureId = data.id;
+		$.get(`/api/ingredients/item/${$("#ingredient-name").text()}`).then(
+			function (item) {
+				var itemId = item.id;
+				var measure = $("#ingredient-measures").val();
+				var quantity = $("#ingredient-quantity").val().trim();
 
-			let ingredient = {
-				id: $("#ingredient-id").text(),
-				quantity: quantity,
-				measure: measure,
-				measure_id: measureId,
-				name: $("#ingredient-name").text(),
-				price: $("#ingredient-price").text(),
-			};
+				$.get(`/api/measures/${measure}`).then(function (data) {
+					let measureId = data.id;
 
-			if (quantity && measure) {
-				$(".ingredients").append(
-					`<tr>
+					let ingredient = {
+						id: itemId,
+						quantity: quantity,
+						measure: measure,
+						measure_id: measureId,
+						name: $("#ingredient-name").text(),
+						price: $("#ingredient-price").text(),
+					};
+
+					if (quantity && measure) {
+						$(".ingredients").append(
+							`<tr>
 						<td class="quantity">${ingredient.quantity}</td>
 						<td class="measure">${ingredient.measure}</td>
 						<td class="name">${ingredient.name}</td>
 						<td>$<span class="price">${ingredient.price}</span></td>
 					</tr>`
-				);
-				recipe.ingredients.push(ingredient);
-				console.log(recipe);
+						);
+						recipe.ingredients.push(ingredient);
+						console.log(recipe);
+					}
+				});
 			}
-		});
+		);
 	});
 
 	// show add manual ingredient if not in db
@@ -210,7 +245,7 @@ $(document).ready(function () {
 				quantity: $("#manual-quantity").val().trim(),
 				measure: $("#manual-measures").val(),
 				measure_id: measureId,
-				name: $("#manual-name").val().trim(),
+				name: $("#manual-name").val().trim().toLowerCase(),
 				price: $("#manual-price").val().trim(),
 			};
 
