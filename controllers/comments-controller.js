5255b25325dff53a56b973302eb50c749b910c2b
Tom Haigh <tmhaigh@gmail.com>
2020-11-22 10:36:43 +1100

    created comments database table

---
+++ controllers/comments-controller.js
@@ -0,0 +1,16 @@
+let db = require("../models");
+
+module.exports = function(app) {
+    app.post("/api/add-comment/:recipe_id/:user_id", function(req, res) {
+        let recipe_id = req.params.recipe_id;
+        let user_id = req.params.user_id;
+        let comment = req.body.comment;
+        db.Comments.create({
+            comment: comment,
+            recipe_id: recipe_id,
+            user_id: user_id
+        }).then(function(data) {
+            res.redirect(`/view-recipe/${recipe_id}`);
+        })
+    })
+}
