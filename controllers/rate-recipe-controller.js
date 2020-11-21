8a0ca2206d75c16c6f95abdd574d5490b90ecd37
Tom Haigh <tmhaigh@gmail.com>
2020-11-21 16:18:42 +1100

    rate-recipe post rating route created

---
+++ controllers/rate-recipe-controller.js
@@ -0,0 +1,18 @@
+let db = require("../models");
+const sequelize = require("sequelize");
+
+module.exports = function(app) {
+    app.post("/api/rate-recipe/:recipe_id/:user_id", function(req, res) {
+        let recipe_id = req.params.recipe_id;
+        let user_id = req.params.user_id;
+        let rating = req.body.rating;
+        db.sequelize.query(`
+        INSERT INTO ratings (rating, createdAt, updatedAt, recipe_id, user_id)
+        VALUES (${rating}, NOW(), NOW(), ${recipe_id}, ${user_id});
+        `, {
+            type: sequelize.QueryTypes.INSERT
+        }).then(function(data) {
+            res.redirect(`/view-recipe/${recipe_id}`);
+        })
+    })
+}
