23d39f109147bfc655a461e46b3447d59b27273e
Simone Cheah <simone_cheah@hotmail.com>
2020-11-23 04:41:04 +1100

    select all from recipe id and insert update route

--- controllers/recipe-ingredients-controller.js
+++ controllers/recipe-ingredients-controller.js
@@ -6,8 +6,7 @@ module.exports = function(app) {
     // get recipe and ingredients list with the recipe_id
     app.get("/api/recipesIngredients/:recipe_id", function(req, res) {
         db.sequelize.query(`
-        SELECT recipe_ingredients.ingredient_id, recipe_ingredients.recipe_id, recipe_ingredients.createdAt, recipe_ingredients.updatedAt
-            FROM recipe_ingredients
+        SELECT * FROM recipe_ingredients
         WHERE recipe_id = ${req.params.recipe_id};`, {
             type: sequelize.QueryTypes.SELECT
         }).then(function(data) {
@@ -25,13 +24,6 @@ module.exports = function(app) {
         })
     })
 
-    // get all the recipes and ingredients
-    // app.get("/api/recipesIngredients", function (req, res) {
-    //     db.recipe_ingredients.findAll().then(function (data) {
-    //         res.json(data);
-    //     })
-    // });
-
     app.post("/api/recipesIngredients", function(req, res) {
         db.sequelize.query(`
         INSERT INTO recipe_ingredients (quantity, createdAt, updatedAt, recipe_id, ingredient_id, measure_id) VALUES (${req.body.quantity},NOW(), NOW(), ${req.body.recipe_id}, ${req.body.ingredient_id}, ${req.body.measure_id});`, {
@@ -40,6 +32,20 @@ module.exports = function(app) {
             res.json(data);
         })
     });
+
+    app.put("/api/recipesIngredients/:id", function(req, res) {
+        db.sequelize.query(`
+        UPDATE recipe_ingredients
+        SET 
+            quantity = ${req.body.quantity}, 
+            ingredient_id = ${req.body.ingredient_id},
+            measure_id = ${req.body.measure_id}
+        WHERE recipe_id = ${req.params.id};`, {
+            type: sequelize.QueryTypes.UPDATE
+        }).then(function(data) {
+            res.json(data)
+        })
+    })
 
     app.delete("/api/recipesIngredients/:id", function(req, res) {
         db.recipe_ingredients.destroy({
