c291434c837ac96cedc8c915198564484381811a
Tom Haigh <tmhaigh@gmail.com>
2020-11-22 18:04:03 +1100

    added post new favourte route

--- controllers/favourites-controller.js
+++ controllers/favourites-controller.js
@@ -1,5 +1,18 @@
+const { decodeBase64 } = require("bcryptjs");
 let db = require("../models");
+let sequelize = require("sequelize");
 
 module.exports = function(app) {
-
+    app.post("/api/add-favourite/:recipe_id/:user_id", function(req, res) {
+        let recipe_id = req.params.recipe_id;
+        let user_id = req.params.user_id;
+        db.sequelize.query(`
+            INSERT INTO favourites (createdAt, updatedAt, recipe_id, user_id)
+            VALUES (NOW(), NOW(), ${recipe_id}, ${user_id})
+            `, {
+            type: sequelize.QueryTypes.INSERT
+        }).then(function() {
+            res.redirect(`/view-recipe/${recipe_id}`);
+        })
+    })
 }
