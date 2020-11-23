f9e90bed2d187dc528a2f39031374bb6dccd4a1c
Seance <siddharth.dhananjay@gmail.com>
2020-11-23 14:38:45 +1100

    Added styling for the main page

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Getting references to our form and inputs
     var loginForm = $("form.login");
     var usernameInput = $("input#username-input");
     var passwordInput = $("input#password-input");
 
     // When the form is submitted, we validate there's an email and password entered
-    loginForm.on("submit", function(event) {
+    loginForm.on("submit", function (event) {
         event.preventDefault();
         var userData = {
             username: usernameInput.val().trim(),
@@ -25,14 +25,14 @@ $(document).ready(function() {
     // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
     function loginUser(username, password) {
         $.post("/api/login", {
-                username: username,
-                password: password
-            })
-            .then(function() {
+            username: username,
+            password: password
+        })
+            .then(function () {
                 window.location.replace("/members");
                 // If there's an error, log the error
             })
-            .catch(function(err) {
+            .catch(function (err) {
                 console.log(err);
             });
     }
